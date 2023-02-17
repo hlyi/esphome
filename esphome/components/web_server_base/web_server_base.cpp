80b0087ef4cd9ed826609cf75fb8da1a16cc7470
H Yi <hlyi@yahoo.com>
2023-02-17 09:27:20 -0600

    update to 2023.6.3 release

--- esphome/components/web_server_base/web_server_base.cpp
+++ esphome/components/web_server_base/web_server_base.cpp
@@ -14,6 +14,10 @@
 #endif
 #endif
 
+#ifdef USE_ESP_IDF
+#include "esphome/components/web_server_idf/web_server_idf_ota.h"
+#endif
+
 namespace esphome {
 namespace web_server_base {
 
@@ -36,6 +40,8 @@ void report_ota_error() {
   StreamString ss;
   Update.printError(ss);
   ESP_LOGW(TAG, "OTA Update failed! Error: %s", ss.c_str());
+#else
+  ESP_LOGW(TAG, "OTA Update failed! Error: %d", Update.get_error());
 #endif
 }
 
@@ -56,6 +62,9 @@ void OTARequestHandler::handleUpload(AsyncWebServerRequest *request, const Strin
       Update.abort();
     }
     success = Update.begin(UPDATE_SIZE_UNKNOWN, U_FLASH);
+#endif
+#ifdef USE_ESP_IDF
+    success = Update.begin(OTA_SIZE_UNKNOWN);
 #endif
     if (!success) {
       report_ota_error();
@@ -100,9 +109,13 @@ void OTARequestHandler::handleRequest(AsyncWebServerRequest *request) {
   if (!Update.hasError()) {
     response = request->beginResponse(200, "text/plain", "Update Successful!");
   } else {
+#ifdef USE_ARDUINO
     StreamString ss;
     ss.print("Update Failed: ");
     Update.printError(ss);
+#else
+    std::string ss = str_sprintf("Update failed! Error: %d", Update.get_error());
+#endif
     response = request->beginResponse(200, "text/plain", ss);
   }
   response->addHeader("Connection", "close");
--- esphome/components/web_server_idf/__init__.py
+++ esphome/components/web_server_idf/__init__.py
@@ -1,5 +1,8 @@
 from esphome.components.esp32 import add_idf_sdkconfig_option
 import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.core import CORE
+from esphome.const import CONF_WEB_SERVER, CONF_OTA
 
 CODEOWNERS = ["@dentra"]
 
@@ -14,3 +17,6 @@ AUTO_LOAD = ["web_server"]
 async def to_code(config):
     # Increase the maximum supported size of headers section in HTTP request packet to be processed by the server
     add_idf_sdkconfig_option("CONFIG_HTTPD_MAX_REQ_HDR_LEN", 1024)
+
+    if CONF_WEB_SERVER in CORE.config and CONF_OTA in CORE.config[CONF_WEB_SERVER]:
+        cg.add_define("USE_WEBSERVER_IDF_MULTIPART")
