48a2389139035e2ad3ffe0e4b388f1022e3fae38
H Yi <hlyi@yahoo.com>
2023-02-17 07:25:11 -0600

    add mqtt device info extraction from project/name

--- esphome/components/mqtt/mqtt_component.cpp
+++ esphome/components/mqtt/mqtt_component.cpp
@@ -153,8 +153,8 @@ bool MQTTComponent::send_discovery_() {
         device_info[MQTT_DEVICE_IDENTIFIERS] = get_mac_address();
         device_info[MQTT_DEVICE_NAME] = node_friendly_name;
         device_info[MQTT_DEVICE_SW_VERSION] = "esphome v" ESPHOME_VERSION " " + App.get_compilation_time();
-        device_info[MQTT_DEVICE_MODEL] = ESPHOME_BOARD;
-        device_info[MQTT_DEVICE_MANUFACTURER] = "espressif";
+        device_info[MQTT_DEVICE_MODEL] = USE_MQTT_DEVICE_MODEL;
+        device_info[MQTT_DEVICE_MANUFACTURER] = USE_MQTT_DEVICE_MANUFACTURER;
         device_info[MQTT_DEVICE_SUGGESTED_AREA] = node_area;
       },
       this->qos_, discovery_info.retain);
