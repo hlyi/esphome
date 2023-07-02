0f2880825cc21520c018d4397ca55c75d0f260f7
HL Yi <hlyi@yahoo.com>
2023-07-01 19:18:49 -0500

    restore commented out code

--- esphome/components/ethernet/ethernet_component.cpp
+++ esphome/components/ethernet/ethernet_component.cpp
@@ -81,7 +81,6 @@ void EthernetComponent::setup() {
       this->phy_ = esp_eth_phy_new_ip101(&phy_config);
       break;
     }
-/*
     case ETHERNET_TYPE_JL1101: {
       this->phy_ = esp_eth_phy_new_jl1101(&phy_config);
       break;
@@ -95,7 +94,6 @@ void EthernetComponent::setup() {
 #endif
       break;
     }
-*/
     default: {
       this->mark_failed();
       return;
